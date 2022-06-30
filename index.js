edc09e8b66d6079ac9cf2a603ded7c655e269a5f
ambishalsaha01 <bishalsaha9989@gmail.com>
2022-06-30 21:02:13 +0600

    Database connection

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
 const express = require('express')
 const app = express()
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
-
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+// Database Integration
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wybo3.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const taskCollection = client.db('todo_Task').collection('task');
+
+        // 
+        
+
+
+    }
+    finally {
+
+    }
+}
 
 
 
